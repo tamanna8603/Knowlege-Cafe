8b9819d9d79a7524ca0b093b884b42d36698333a
Tamanna Akter <tamanna25-030@diu.edu.bd>
2023-09-14 12:34:48 +0600

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+import './App.css'
+import Header from './components/Header/Header'
+import Blogs from './components/Blogs/Blogs'
+
+function App() {
+  
+
+  return (
+    <>
+      
+      <Header></Header>
+      <Blogs></Blogs>
+    
+    </>
+  )
+}
+
+export default App
---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,9 @@
+const Header = () => {
+  return (
+    <div>
+      <h1 className=' text-4xl font-bold'>React Knowledge Cafe</h1>
+    </div>
+  );
+};
+
+export default Header;
